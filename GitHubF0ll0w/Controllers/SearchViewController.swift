30361015e775f130f951cfa1d9997ce6e0bf08cf
9OSU1G02 <huyquocnguyen1902@gmail.com>
2020-11-28 13:36:12 +0700

    Handle keyboard obscure	get followers & get following button

--- GitHubF0ll0w/Controllers/SearchViewController.swift
+++ GitHubF0ll0w/Controllers/SearchViewController.swift
@@ -11,10 +11,7 @@ class SearchViewController: UIViewController {
     
     
     // MARK: - Properties
-    let scrollView = UIScrollView()
-    
-    let contentView = UIView()
-    
+   
     let logo = UIImageView()
     
     let searchTextFiled = TextField()
@@ -27,6 +24,7 @@ class SearchViewController: UIViewController {
         searchTextFiled.delegate = self
         view.backgroundColor = .systemBackground
         layoutUI()
+        listenForKeyboardNotification()
     }
     
     
@@ -38,43 +36,23 @@ class SearchViewController: UIViewController {
     
     private func layoutUI() {
         
-        configureScrollView()
         configureLogo()
         configureTextFiled()
         configureButton()
         
     }
     
-    
-    private func configureScrollView() {
-        
-        view.addSubview(scrollView)
-        scrollView.pinToEdge(of: view)
-        
-        scrollView.addSubview(contentView)
-        contentView.pinToEdge(of: scrollView)
-        
-        
-        NSLayoutConstraint.activate([
-            
-            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
-            contentView.heightAnchor.constraint(equalToConstant: 700),
-            
-        ])
-        
-    }
-    
-    
     private func configureLogo() {
         
         logo.image = Images.logo
-        contentView.addSubview(logo)
-        logo.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(logo)
         
+        logo.translatesAutoresizingMaskIntoConstraints = false
+        let topConstraintConstant: CGFloat = DeviceTypes.isiPhoneSE || DeviceTypes.isiPhoneSE2nd || DeviceTypes.isiPhone8Zoomed ? 20 : 90
         NSLayoutConstraint.activate([
             
-            logo.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 80),
-            logo.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            logo.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: topConstraintConstant),
+            logo.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             logo.widthAnchor.constraint(equalToConstant: 200),
             logo.heightAnchor.constraint(equalToConstant: 200),
             
@@ -85,16 +63,16 @@ class SearchViewController: UIViewController {
     
     private func configureTextFiled() {
         
-        contentView.addSubview(searchTextFiled)
+        view.addSubview(searchTextFiled)
         
         let padding: CGFloat = 50
         
         NSLayoutConstraint.activate([
             
-            searchTextFiled.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            searchTextFiled.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             searchTextFiled.topAnchor.constraint(equalTo: logo.bottomAnchor, constant: 48),
-            searchTextFiled.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
-            searchTextFiled.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            searchTextFiled.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            searchTextFiled.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
             searchTextFiled.heightAnchor.constraint(equalToConstant: 50)
             
         ])
@@ -104,23 +82,47 @@ class SearchViewController: UIViewController {
     
     private func configureButton() {
         
-        contentView.addSubviews(getFollowersButton, getFollowingButton)
+        view.addSubviews(getFollowersButton, getFollowingButton)
         
-        let padding: CGFloat = 65
+        let padding: CGFloat = 60
         let height: CGFloat = 50
         NSLayoutConstraint.activate([
             
-            getFollowingButton.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -padding),
-            getFollowingButton.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
-            getFollowingButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            getFollowingButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -padding),
+            getFollowingButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            getFollowingButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
             getFollowingButton.heightAnchor.constraint(equalToConstant: height),
             
             getFollowersButton.bottomAnchor.constraint(equalTo: getFollowingButton.topAnchor, constant: -padding),
-            getFollowersButton.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
-            getFollowersButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            getFollowersButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            getFollowersButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
             getFollowersButton.heightAnchor.constraint(equalToConstant: height)
             
         ])
+        
+    }
+    
+    
+    
+    private func listenForKeyboardNotification() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+    
+    
+    
+    @objc func keyboardWillChange(notification: Notification) {
+        
+        guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else { return }
+        
+        
+        if notification.name == UIResponder.keyboardWillShowNotification || notification.name == UIResponder.keyboardWillChangeFrameNotification {
+            view.frame.origin.y = -keyboardSize.height
+        }
+        else {
+            view.frame.origin.y = 0
+        }
         
     }
     
